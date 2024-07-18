4233ba9082f44bc357e63280ef715268ceacc7b4
Vinita <vinitagurnani6@gmail.com>
2024-07-19 00:31:35 +0530

    Added Routes to Home About TextUtils

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import About from "./Components/About";
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import Alert from "./Alert";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -23,22 +30,29 @@ function App() {
       setMode ('dark');
       document.body.style.backgroundColor='#042743';
       showAlert ("Dark Mode enabled", "success");
+      document.title = "TextUtils - Dark mode"
     }
     else {
       setMode ('light');
       document.body.style.backgroundColor = 'white'
       showAlert("Light mode enabled", "success");
+      document.title = "TextUtils - Light mode"
+
     }
    }
   return (
     <>
-      <Navbar title = "TextUtils" mode = {mode} toggleMode = {toggleMode}/>
-      <Alert alert = {alert}/>
-      <div className="container">
-        <TextForm heading = "Enter the text to analyze" mode = {mode} showAlert = {showAlert}></TextForm>
-        {/* <About></About> */}
+    <Router>
+      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        <Routes>
+          <Route path="/about" element={<About/>} />
+          <Route path="/" element={<TextForm heading="Enter the text to analyze" mode={mode} showAlert={showAlert} />} />
+        </Routes>
       </div>
-    </>
+    </Router>
+  </>
   );
 }
 // everthing which is written inside the return statement is JSX., app.js = javascript file. JSX is 95% html, in {} = javascript.
